bd88296e8f7ff696d34791ded369cb061d39bf49
Andrey <adwaises@mail.ru>
2018-05-04 15:37:09 +0400

    появился журнал

--- KPO_System/KPO_System/FormTeacher.cs
+++ KPO_System/KPO_System/FormTeacher.cs
@@ -13,9 +13,14 @@ namespace KPO_System
 {
     public partial class FormTeacher : Form
     {
+        string discipline = "Russkiy Yazyk";
         private DataTable dt;
+        private DataTable dtMarks;
         NpgsqlDataAdapter da;
         NpgsqlConnection conn;
+
+        List<int> listID = new List<int>();
+
         public FormTeacher(NpgsqlConnection connect, string login)
         {
             InitializeComponent();
@@ -45,6 +50,8 @@ namespace KPO_System
             {
                 CBClass.Items.Add(dt.Rows[i][0].ToString());
             }
+
+            //запрос дисциплины по учителю
 
         }
 
@@ -89,9 +96,11 @@ namespace KPO_System
             CBLetter.Items.Clear();
             conn.Open(); //Открываем соединение.
 
-            //string sql = "set names 'windows-1251'; SELECT * FROM teacher";
+            //получили список класса
 
-            string sql = String.Format(" select ", Convert.ToInt32(CBClass.Text));
+            string sql = String.Format(" select id_pupil, famil, pupil.name, otchestvo from pupil "+
+"join class on class.id_class = pupil.id_class "+
+"where number = {0} and letter = '{1}'; ", Convert.ToInt32(CBClass.Text), CBLetter.Text);
 
             da = new NpgsqlDataAdapter(sql, conn);
             dt = new DataTable();
@@ -100,6 +109,42 @@ namespace KPO_System
             //dataGridView1.DataSource = dt;
 
             conn.Close(); //Закрываем соединение.
+
+            dt.Columns.Add("Оценка");
+
+            // получили оценки по предмету
+
+            conn.Open(); //Открываем соединение.
+
+            //получили список оценок
+
+            sql = String.Format("select id_pupil, mark from performance "+
+"join discipline on discipline.id_discipline = Performance.id_discipline "+
+"where discipline.name = '{0}';",discipline);
+
+            da = new NpgsqlDataAdapter(sql, conn);
+            dtMarks = new DataTable();
+            da.Fill(dtMarks);
+            conn.Close(); //Закрываем соединение.
+
+            //находим соотвествия
+            for(int i=0; i< dtMarks.Rows.Count; i++)
+            {
+                for(int j=0;j<dt.Rows.Count; j++)
+                {
+                    if((dt.Rows[j][0]).ToString() == dtMarks.Rows[i][0].ToString())
+                    {
+                        dt.Rows[j][4] = dtMarks.Rows[i][1];
+                    }
+                }
+            }
+            //сохраняем порядок id и удаляем столбец
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                listID.Add(Convert.ToInt32( dt.Rows[i][0]));
+            }
+
+            dt.Columns.RemoveAt(0);
 
             dataGridView1.DataSource = dt;
         }
